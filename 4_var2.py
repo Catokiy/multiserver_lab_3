f133d140f64cbe164280c7deaa08887a3a206eb6
Lemacoder <ladygin.anastasia@yandex.ru>
2024-03-28 13:27:16 +0300

    4 var finish

--- 4_var2.py
+++ 4_var2.py
@@ -1,10 +1,4 @@
-#2) Реализовать программу 2 и модифицировать программу 1 так, чтоб при условии, что
-#программа 1 запущена 2 программа устанавливает сетевое взаимодействие с программой 1.
-#Пользователь 2 программы может отправить команду 1 программе для обновления
-#информации о программах и получения соответствующего файла с данными.
-
-
-import socket #для работы с сетью
+import socket 
 import json
 import pickle
 
@@ -13,16 +7,11 @@ def saving():
     with open('pr_inf.json', 'w', encoding = "UTF-8") as file:
         json.dump(pickle.loads(data, encoding="utf-8"), file, indent=4, ensure_ascii=False)
         file.close()
-        
-        #json.dump(programs_info, file, indent=4, ensure_ascii=False)
-        #file.close()
         print("File was saved")
- #устанавливает связь с другой программой 
 host = 'localhost'
 port = 9090
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #создаём канал связи, казываем тип, протокол. 
-#! связь будет закрыта автоматически после завершения работы!
-s.connect((host, port)) #устанавливаем связь 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.connect((host, port)) 
 flag = True
 while True:
     m = input("Please, input your command ")
@@ -43,10 +32,3 @@ while True:
         saving()
         print("Success!!!")
 s.close()
-
- #принимаем данные от удалённого хоста, серез соединение.
-   
-        #Метод блокирует выполнение программы, пока не будет полученно указанное кол-во байт данных
-
-
-
